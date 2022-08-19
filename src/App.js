0c16e0115d2d7dac7219f1a1671b0607cdd65cc5
iamlucasgomes <lucas.devjs@gmail.com>
2022-08-19 02:14:17 -0300

    UPLOAD: files solar system

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
+import Footer from './components/Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SolarSystem />
+        <Missions />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
